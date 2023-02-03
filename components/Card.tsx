3858669e62b8060d49a15c7fd9debb6e7b64fe74
unknown <rida19974@gmail.com>
2023-02-04 01:15:43 +0200

    CHORE add horizontal row

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
 
-const Card = () => {
-  return <div>Card</div>;
+interface CardProps {
+  image: StaticImageData;
+  number: string;
+  title: string;
+  paragraph: string;
+}
+
+const Card = ({ image, number, title, paragraph }: CardProps) => {
+  return (
+    <div className="flex gap-[25px]">
+      <Image src={image} width={100} height={128} alt="labtop" />
+      {/* {image.icon} */}
+      <div>
+        <p className="text-[25px] text-GrayishBlue font-bold">{number}</p>
+        <h3 className="text-[19px] text-VeryDarkBlue font-bold">{title}</h3>
+        <p className="text-DarkGrayishBlue md:max-w-[223px]">{paragraph}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Card from "../components/Card";
+import firstPhoto from "public/assets/images/image-retro-pcs.jpg";
+import secondPhoto from "public/assets/images/image-top-laptops.jpg";
+import thirdPhoto from "../public/assets/images/image-gaming-growth.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +22,25 @@ export default function Home() {
       <main className={inter.className}>
         <Header />
         <Hero />
-        <div className="container">
-          <Card />
+        <div className="container flex flex-col md:flex-row md:justify-between md:items-center mt-[64px] md:mt-[72px] pb-[78px] gap-[33px] md:gap-0">
+          <Card
+            image={firstPhoto}
+            number="01"
+            title="Reviving Retro PCs"
+            paragraph="What happens when old PCs are given modern upgrades?"
+          />
+          <Card
+            image={secondPhoto}
+            number="02"
+            title="Top 10 laptops of 2022"
+            paragraph="Our best picks for various needs and budgets."
+          />
+          <Card
+            image={thirdPhoto}
+            number="03"
+            title="The Growth of Gaming"
+            paragraph="How the pandemic has sparked fresh opportunities."
+          />
         </div>
       </main>
     </>
