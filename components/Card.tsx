9bf82b5dd9586df00a24d7d68828f24c17a7b7a2
unknown <rida19974@gmail.com>
2023-02-04 14:28:50 +0200

    CHORE modify pic in footer

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,16 +12,16 @@ const Card = ({ image, number, title, paragraph }: CardProps) => {
   return (
     <div className="flex gap-[25px] group cursor-pointer ">
       <div className="overflow-hidden">
+        {/* {image.icon} */}
         <Image
-          className="group-hover:scale-125 group-hover:rotate-12 transition-all duration-200 min-h-full"
+          className="group-hover:scale-125 group-hover:rotate-12 transition-all duration-200 min-h-full  object-cover"
           src={image}
           width={100}
           height={128}
           alt="labtop"
         />
       </div>
-      {/* {image.icon} */}
-      <div>
+      <div className="flex-1">
         <p className="text-[25px] text-GrayishBlue font-bold">{number}</p>
         <h3 className="text-[19px] text-VeryDarkBlue font-bold   transition-all duration-200 group-hover:text-SoftRed">
           {title}
